25abef3ae017c2e6ce7dc77914ce613b83e6bed1
Pawel Slusarz <p@sw7d.com>
2016-02-25 09:48:29 -0600

    padding low numbers

--- src/main/groovy/com/sw7d/RadomArchiveDownloader.groovy
+++ src/main/groovy/com/sw7d/RadomArchiveDownloader.groovy
@@ -10,7 +10,9 @@ class RadomArchiveDownloader {
         def result
         //(4243..6000).each { index ->
         //[4243, 4244, 4245, 4246, 4278, 4279, 4280, 4281, 4282, 4283, 4314, 4315, 4316, 4317, 4318, 4319, 4320, 4321, 4322, 4323, 4584, 4585, 4586, 4587, 4996, 4997, 5127, 5231, 5240, 5258, 5272, 5300, 5306, 5325].each {index ->
-        (11732..20000).each {index ->
+        //(15963..30000).each {index ->
+        (29230..35000).collect{"$it".padLeft(4, "0")}.each {index ->
+
             try {
                 result = Command.run("curl http://bc.mbpradom.pl/Content/${index}/ -v")
                 while (result.retryLater) {
