0629d55ee2287f811fe11e139c2004db34f7dd65
Pawel Slusarz <p@sw7d.com>
2016-03-15 00:54:29 -0500

    recognize most of zycie radomskie

--- src/main/groovy/com/sw7d/RadomDocumentConstructor.groovy
+++ src/main/groovy/com/sw7d/RadomDocumentConstructor.groovy
@@ -19,7 +19,11 @@ class RadomDocumentConstructor {
                 }
                 xml << '  <field name = "content"><![CDATA['+System.properties['line.separator']
                 file.eachLine { String line ->
-                    xml << StringEscapeUtils.escapeXml11(line)+System.properties['line.separator']
+                    if (line.endsWith('¬') || line.endsWith('-')) {
+                        xml << StringEscapeUtils.escapeXml11(line.substring(0, line.length()-1))
+                    } else {
+                        xml << StringEscapeUtils.escapeXml11(line) + System.properties['line.separator']
+                    }
                 }
                 xml << '  ]]></field>'+System.properties['line.separator']
                 xml << "</doc></add>"
@@ -35,11 +39,42 @@ class RadomDocumentConstructor {
         Map<String, String> result = [:]
         if (fileName.toLowerCase().startsWith('zycie%20radomskie')) {
             def chunks = (fileName - '.txt').split('-')
-            result.series_name = 'Zycie Radomskie'
+            result.series_name = 'Życie Radomskie'
             result.year = Integer.valueOf(chunks[1])
             result.index_in_year = Integer.valueOf(chunks[2])
             result.series_index = Integer.valueOf(""+chunks[1]+chunks[2].padLeft(5,'0'))
-            result.id = 'Zycie Radomskie, rok: '+result.year+", nr. "+chunks[2]
+            result.id = 'Życie Radomskie, rok: '+result.year+", nr. "+chunks[2]
+            result.file_name = "zycie_radomskie_"+result.year+"_"+result.index_in_year
+            result.url_file_name = fileName - ".txt"
+            result.original_file_extension = "djvu"
+            //println result
+        } else if (fileName.toLowerCase().startsWith("wszech%c5%9bwiat-")) { //Wszech%C5%9Bwiat-1887-27-00001 or Wszech%C5%9Bwiat-1887-spis-00001
+            def chunks = (fileName - '.txt').split('-')
+            result.series_name = 'Wszechświat'
+            result.year = Integer.valueOf(chunks[1])
+            try {
+                result.index_in_year = Integer.valueOf(chunks[2])
+            } catch (NumberFormatException e) {
+                result.index_in_year = 0
+            }
+            result.series_index = Integer.valueOf(""+chunks[1]+(result.index_in_year+"").padLeft(5,'0'))
+            result.id = 'Wszechświat, rok: '+result.year+", nr. "+chunks[2]
+            result.file_name = "wszechswiat_"+result.year+"_"+result.index_in_year
+            result.url_file_name = fileName - ".txt"
+            result.original_file_extension = "djvu"
+            //println result
+        } else if(fileName.toLowerCase().startsWith("%c5%bbycie%20radomskie-")) { //%C5%BBycie%20Radomskie-1967-nr68
+            def chunks = (fileName - '.txt').split('-')
+            result.series_name = 'Życie Radomskie'
+            try {
+                result.year = Integer.valueOf(chunks[1])
+                result.index_in_year = Integer.valueOf(chunks[2] - 'nr')
+            } catch (NumberFormatException e) {
+                result.year = Integer.valueOf(chunks[1].split('%20')[0])
+                result.index_in_year = Integer.valueOf(chunks[1].split('%20')[1])
+            }
+            result.series_index = Long.valueOf(""+result.year+""+(result.index_in_year+"").padLeft(7,'0'))
+            result.id = 'Życie Radomskie, rok: '+result.year+", nr. "+result.index_in_year
             result.file_name = "zycie_radomskie_"+result.year+"_"+result.index_in_year
             result.url_file_name = fileName - ".txt"
             result.original_file_extension = "djvu"
